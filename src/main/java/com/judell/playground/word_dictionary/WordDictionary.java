e350e8cfb558553eaea7bd2a507e2a1be763dc99
Justin <justintdavis8@gmail.com>
2021-10-23 00:03:59 -0500

    Refactored logic flow

--- src/main/java/com/judell/playground/word_dictionary/WordDictionary.java
+++ src/main/java/com/judell/playground/word_dictionary/WordDictionary.java
@@ -12,54 +12,39 @@ import org.springframework.web.client.RestTemplate;
 public class WordDictionary {
 
     public static void main(String[] args) throws JsonProcessingException {
+        owlDictionary("potato");
         owlDictionary("ham");
+        owlDictionary("garfunkle");
 //        freeDictionary("hello");
     }
 
-    private static void owlDictionary(String word) throws JsonProcessingException {
+    private static void owlDictionary(String word) {
         String previousWord = "hush";
 
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-//        ObjectMapper mapper = new ObjectMapper();
         HttpEntity<OwlWordModel> httpEntity = new HttpEntity<>(headers);
 
         headers.set("Authorization", "Token 51fd1edd0479b71faea8a096e460b5e687c4e9da");
 
+        String lastCharPrevWord = previousWord.substring(previousWord.length() - 1);
+        String firstLetterCurrentWord = String.valueOf(word.charAt(0));
+
         try {
-            restTemplate.exchange(
+            ResponseEntity<OwlWordModel> response = restTemplate.exchange(
                     "https://owlbot.info/api/v4/dictionary/" + word,
                     HttpMethod.GET,
                     httpEntity,
                     OwlWordModel.class
             );
+
+            if (lastCharPrevWord.equalsIgnoreCase(firstLetterCurrentWord) && word.equalsIgnoreCase(response.getBody().getWord())) {
+                System.out.println(word + " is a Correct");
+            } else {
+                System.out.println(word + " does not begin with" + " " + lastCharPrevWord);
+            }
         } catch (HttpClientErrorException httpClientErrorException) {
-            System.out.println("The word you are looking for does not exist or your spelling is wrong");
+            System.out.println("The word you are looking for does not exist or your spelling is wrong: " + word);
         }
-
-        String lastCharPrevWord = previousWord.substring(previousWord.length() - 1);
-        String firstLetterCurrentWord = String.valueOf(word.charAt(0));
-
-        if (lastCharPrevWord.equalsIgnoreCase(firstLetterCurrentWord)) {
-            System.out.println("Correct");
-            return;
-        }
-
-        System.out.println("This word does not begin with" + " " + lastCharPrevWord);
-
-//        String jsonResp = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(new OwlWordResponse(response.getBody()));
-//        System.out.println(response.getStatusCode());
-//        System.out.println(jsonResp);
     }
-
-//    private static void freeDictionary(String word) throws JsonProcessingException {
-//        RestTemplate restTemplate = new RestTemplate();
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        ResponseEntity<FreeWords> response = restTemplate.getForEntity("https://api.dictionaryapi.dev/api/v2/entries/en/" + word, FreeWords.class);
-//
-//        String jsonResp = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(new FreeWordResponse(response.getBody().getWordModels()));
-//        System.out.println(response.getStatusCode());
-//        System.out.println(jsonResp);
-//    }
 }
