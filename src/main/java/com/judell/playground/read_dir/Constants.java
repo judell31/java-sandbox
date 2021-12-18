c87fc303601734e51689cdcca3da7c94b04827fd
Justin Davis <justintdavis8@gmail.com>
2021-12-18 12:23:57 -0600

    Added ability to read the contents of a directory

---
+++ src/main/java/com/judell/playground/read_dir/Constants.java
@@ -0,0 +1,5 @@
+package com.judell.playground.read_dir;
+
+public class Constants {
+    public static String DIR_PATH = System.getenv("DIR_PATH");
+}
--- src/main/java/com/judell/playground/read_dir/ReadDir.java
+++ src/main/java/com/judell/playground/read_dir/ReadDir.java
@@ -1,8 +1,23 @@
 package com.judell.playground.read_dir;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class ReadDir {
 
     public static void main(String[] args) {
-        System.out.println("Read Dir");
+        System.out.println("Read Dir\n");
+
+        Path dir = Paths.get(Constants.DIR_PATH);
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path file: stream) {
+                System.out.println("URI: " + file.toUri());
+                System.out.println("File: " + file.toFile());
+            }
+        } catch (IOException | DirectoryIteratorException x) {
+            // IOException can never be thrown by the iteration.
+            // In this snippet, it can only be thrown by newDirectoryStream.
+            System.err.println(x);
+        }
     }
 }
