fee26c152cb980acdd4615d51d5ec24f7f4eae1c
Justin Davis <justintdavis8@gmail.com>
2022-05-04 02:27:58 -0500

    Added word owl request

---
+++ src/main/java/com/judell/playground/api_connection/DefinitionListModel.java
@@ -0,0 +1,29 @@
+package com.judell.playground.api_connection;
+
+public class DefinitionListModel {
+    private String type;
+    private String definition;
+    private String example;
+    private String image_url;
+    private String emoji;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public String getEmoji() {
+        return emoji;
+    }
+}
