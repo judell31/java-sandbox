0ec78d6e171cccfaab748cc52c5bf9130afc2b19
Justin Davis <justintdavis8@gmail.com>
2022-08-16 13:53:01 -0500

    Enum implementation

---
+++ src/main/java/com/judell/playground/enum_constant/DisplayEnum.java
@@ -0,0 +1,8 @@
+package com.judell.playground.enum_constant;
+
+public class DisplayEnum {
+
+    public static void main(String[] args) {
+        System.out.println(EnumConstants.USER_ADDED);
+    }
+}
---
+++ src/main/java/com/judell/playground/enum_constant/EnumConstants.java
@@ -0,0 +1,8 @@
+package com.judell.playground.enum_constant;
+
+import lombok.Getter;
+
+@Getter
+public enum EnumConstants {
+    USER_ADDED,
+}
