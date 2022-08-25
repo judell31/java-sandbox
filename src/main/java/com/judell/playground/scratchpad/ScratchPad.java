ef4e459b76d22b8146b90db6b3d0d3d34183c02f
Justin Davis <justintdavis8@gmail.com>
2022-08-25 00:42:02 -0500

    Create ScratchPad.java

---
+++ src/main/java/com/judell/playground/scratchpad/ScratchPad.java
@@ -0,0 +1,9 @@
+package com.judell.playground.scratchpad;
+
+public class ScratchPad {
+    
+    public static void main(String[] args) {
+        System.out.println("I am a scratch pad");
+        System.out.println("----------------------------------------");
+    }
+}
