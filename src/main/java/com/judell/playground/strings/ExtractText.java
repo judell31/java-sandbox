8905be8dfce927cbdd60271ac9b5f8598d2579d5
Justin Davis <justintdavis8@gmail.com>
2022-10-01 13:06:14 -0500

    Extract text example

---
+++ src/main/java/com/judell/playground/strings/ExtractText.java
@@ -0,0 +1,18 @@
+package com.judell.playground.strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractText {
+
+    public static void main(String[] args) {
+        String str = "[Cum in me]";
+
+        Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+        Matcher matcher = pattern.matcher(str);
+
+        if( matcher.find() ) {
+            System.out.println( matcher.group(1));
+        }
+    }
+}
