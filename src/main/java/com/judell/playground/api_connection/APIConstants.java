ea45affb6579d388e3fa4b60a830d9a221a07d3a
Justin Davis <justintdavis8@gmail.com>
2022-10-13 16:48:00 -0500

    Updated api connection package with correct values

--- src/main/java/com/judell/playground/api_connection/APIConstants.java
+++ src/main/java/com/judell/playground/api_connection/APIConstants.java
@@ -2,7 +2,10 @@ package com.judell.playground.api_connection;
 
 public class APIConstants {
     public static final String GET_URL = System.getenv("GET_URL");
+    public static final String OWL_URL = System.getenv("OWL_URL");
     public static final String POST_URL = System.getenv("POST_URL");
     public static final String PUT_URL = System.getenv("PUT_URL");;
     public static final String DELETE_URL = System.getenv("DELETE_URL");
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String OWL_TOKEN = System.getenv("OWL_TOKEN");
 }
