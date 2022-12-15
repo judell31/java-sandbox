96c9f6ca1c42743b15b4aa749818e6cb31e2a92e
Justin Davis <justintdavis8@gmail.com>
2022-12-15 12:58:09 -0600

    Formatted code

--- src/main/java/com/judell/playground/api_connection/APIConstants.java
+++ src/main/java/com/judell/playground/api_connection/APIConstants.java
@@ -4,7 +4,7 @@ public class APIConstants {
     public static final String GET_URL = System.getenv("GET_URL");
     public static final String OWL_URL = System.getenv("OWL_URL");
     public static final String POST_URL = System.getenv("POST_URL");
-    public static final String PUT_URL = System.getenv("PUT_URL");;
+    public static final String PUT_URL = System.getenv("PUT_URL");
     public static final String DELETE_URL = System.getenv("DELETE_URL");
     public static final String AUTH_HEADER = "Authorization";
     public static final String OWL_TOKEN = System.getenv("OWL_TOKEN");
